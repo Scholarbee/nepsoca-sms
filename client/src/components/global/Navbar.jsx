6f2c870fa84a397d839533cff437904fb2b34526
Scholarbee <scholarbee6@gmail.com>
2024-06-04 23:45:10 +0000

    first commit

---
+++ client/src/components/global/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <img className="logo" src={"/sts-logo2.png"} alt="galleryImage" />
+      <p>SCHOOL MANAGEMENT</p>
+    </header>
+  );
+};
+
+export default Navbar;
